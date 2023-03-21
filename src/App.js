186fe163fd7fff1f11a015ee0ea3c502f34d3828
Ritik Ranjan singh <ritiksinghlive@gmail.com>
2023-03-21 16:10:14 +0530

    dark mode button done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 
 import Navbar from './components/Navbar';
 import Textfrom from './components/Textform';
+import About from './components/About';
+import React, { useState } from 'react'
+
 function App() {
+
+  const [mode,setMode]=useState("light")
+  const toggleMode=()=>{
+   
+    if(mode==='dark'){
+      setMode("white");
+      console.log('hello')
+      document.body.style.backgroundColor='white';
+      document.body.style.Color='black';
+     
+    }
+    else{
+      setMode("dark");
+      document.body.style.backgroundColor="#042743";
+      document.body.style.Color="white";
+   
+    }
+  }
   return (
     <>
- <Navbar title="TextUtils"/>
- <Textfrom heading="Enter the Text to analyze"/>
+ <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+ <Textfrom heading="Enter the Text to analyze" mode={mode}/>
+       {/* <About/> */}
     </>
     
     
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 
 
 function Textfrom(props){
-
+    const [text,setText]=useState("");
 
    const  handleUpperClick=()=>{
         
@@ -14,21 +14,41 @@ function Textfrom(props){
     const handleOnChange=(event)=>{
            setText(event.target.value);
     }
-    const [text,setText]=useState("Enter the text here");
    
+    const handleLowerClick=()=>{
+         const lower=text.toLowerCase();
+         setText(lower);
+    }
+    const handleClerClick=()=>{
+        setText("");
+    }
+    const handlePreview=()=>{
+       let p=document.getElementById("preview");
+        p.innerHTML=text;
+    }
+    const handleCopy=()=>{
+          const box=document.getElementById("mybox");
+          box.select();
+          navigator.clipboard.writeText(text.Value);
+    }
     return (
           <>
-       <div className="container">
+       <div className="container"  style={{ color: props.mode==='dark' ? 'white' : '#042743'}}  >
        <div className="mb-3 my-3">
         <h1>{props.heading}</h1>
-  <textarea className="form-control"  value={text}  onChange={handleOnChange}  id="mybox" rows="7"></textarea>
+  <textarea className="form-control"  value={text} style={{ backgroundColor: props.mode==='dark' ? '#042743' : 'white',color: props.mode==='dark' ? 'white' : 'black'}} onChange={handleOnChange} id="mybox" rows="7"></textarea>
 </div>
-<button className='btn btn-primary' onClick={handleUpperClick}>Convert to Uppercase </button>
+<button className='btn btn-primary mx-1' onClick={handleUpperClick}>Convert to Uppercase </button>
+<button className='btn btn-primary mx-1' onClick={handleLowerClick}>Convert to Lowercase </button>
+<button className='btn btn-primary mx-1' onClick={handleClerClick}>Clear</button>
+<button className='btn btn-primary mx-1' onClick={handleCopy}>Copy</button>
        </div>
-       <div className="container my-3">
+       <div className="container my-3" style={{ color: props.mode==='dark' ? 'white' : '#042743'}}>
         <h1>Your text Summary</h1>
-        <p>334 words, 324 character</p>
-        
+        <p>{text.split(" ").length} words, {text.length} character</p>
+        <h2>Preview</h2>
+        <p id="preview"></p>
+        <button className='btn btn-primary mx-1' onClick={handlePreview} >click here to preview</button>
        </div>
 </>
     )
